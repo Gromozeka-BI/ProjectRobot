0469facd42b6a64d953cd465e099ea41066739d1
Gromozeka <Gromozeka@DESKTOP-3HAKCN9>
2022-03-05 10:01:47 +0300

    Добавьте файлы проекта.

---
+++ ProjectRobot/Source.cpp
@@ -0,0 +1,42 @@
+using namespace std;
+
+class Software {
+public:
+	void Start() {
+
+	}
+	void SearchGraffiti() {
+
+	}
+	void Robot() {
+
+	}
+	void MoveToGraffiti() {
+
+	}
+};
+class Camera {
+public:
+	void TurnOnCamera() {
+
+	}
+	void TakePhoto() {
+
+	}
+};
+class Robot {
+public:
+	void GoToGraffiti() {
+
+	}
+	void TurnCleanerOn() {
+
+	}
+
+};
+
+
+int main() {
+
+}
+
