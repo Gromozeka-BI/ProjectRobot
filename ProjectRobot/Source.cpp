e5b8482971aaf4b1427f88d13819bb1dabd8854a
Иван Бояринов <boyarinov.ivan081202@yandex.ru>
2022-03-26 11:59:07 +0300

    26.03.2022

--- ProjectRobot/Source.cpp
+++ ProjectRobot/Source.cpp
@@ -1,39 +1,79 @@
 using namespace std;
 
 class Software {
+private:
+	int Xphoto;
+	int Yphoto;
+	int Xroom;
+	int Yroom;
 public:
-	void Start() {
-
+	void Room() {
+		//Room size
+		//All breakpoint (Width, Height, Length, etc.)
 	}
-	void SearchGraffiti() {
-
+	void Camera() {
+		//Location camera (X,Y,Z)
+		//Angles 
 	}
-	void Robot() {
-
+	void SearchGraffiti(int Xphoto, int Yphoto, int& Xroom, int& Yroom) { 
+		// search for dirt coordinates
+		// translation from the coordinate system of the photo to the coordinate system of the room
 	}
-	void MoveToGraffiti() {
-
+	int GetX() {
+		return Xroom;
+	}
+	int GetY() {
+		return Yroom;
 	}
 };
 class Camera {
+private:
 	Mat png;
+	int x;
+	int y;
 public:
-	void TurnOnCamera() {
-
+	void dirtSearch() {
+		TurnOnCamera();
+		png = TakePhoto();
+		dirtDetected(png, x, y);
 	}
-	void TakePhoto() {
+
+	void TurnOnCamera() {
+		// Camera ON
+	}
+	Mat TakePhoto() {
+		// Take Photo
 		return png;
+	}
+	void dirtDetected(Mat png, int& x, int& y) {
+		// search for dirt coordinates on photo
+	}
+
+	int  GetX() { // Return X on Photo
+		return this->x;
+	}
+
+	int GetY() {// Return Y on Photo
+		return this->y;
 	}
 };
 class Robot {
+private:
+	int x;
+	int y;
 public:
+	void SetX(int Xroom) {
+		this->x = Xroom;
+	}
+	void SetY(int Yroom) {
+		this->y = Yroom;
+	}
 	void GoToGraffiti(int x, int y) {
 
 	}
 	void TurnCleanerOn(bool cleaner) {
 
 	}
-
 };
 
 
